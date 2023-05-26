a8dfb773f6c0fb86bb5854d4263e7dff7597a968
rlanck_21 <127465039+rlandk@users.noreply.github.com>
2023-05-26 08:45:23 +0700

    Add files via upload

---
+++ Final Projek/Projek/script.js
@@ -0,0 +1,20 @@
+function telor() {
+    prompt("masukan jumlah pembelian:")
+    prompt("Masukan Alamat Anda:");
+    prompt("Masukan No Telepon Anda:");
+    alert("Martabak Telor akan segara kami kirim")
+}
+
+function keju() {
+    prompt("masukan jumlah pembelian:")
+    prompt("Masukan Alamat Anda:");
+    prompt("Masukan No Telepon Anda:");
+    alert("Martabak Keju akan segara kami kirim")
+}
+
+function coka() {
+    prompt("masukan jumlah pembelian:")
+    prompt("Masukan Alamat Anda:");
+    prompt("Masukan No Telepon Anda:");
+    alert("Martabak Coklat Kacang akan segara kami kirim")
+}
